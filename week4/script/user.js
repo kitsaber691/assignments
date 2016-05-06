5b3f3ecf397b0a1b0aeed9f5c52c6813148c38ac
Mariel Dunietz <mariel.dunietz@gmail.com>
2016-05-05 17:55:02 -0700

    Submission

---
+++ week4/script/user.js
@@ -0,0 +1,15 @@
+$("document").ready(function () {
+  'use strict';
+  function googleMap() {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementsByTagName('main')[0], {
+      center: {lat: 47.608, lng: -122.33},
+      scrollwheel: true,
+      zoom: 8
+    });
+    marker.
+    var infoWindow = new google.maps.InfoWindow({
+        content: 'UW PCE HQ'
+    });
+  }
+});

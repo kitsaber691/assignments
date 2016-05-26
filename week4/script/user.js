b4b40d6567abb330a8bbcf528a72e016ba4c55e9
Mariel Dunietz <mariel.dunietz@gmail.com>
2016-05-26 16:17:25 -0700

    Troubleshooting Google Maps API

--- week4/script/user.js
+++ week4/script/user.js
@@ -1,15 +1,15 @@
 $("document").ready(function () {
   'use strict';
   function googleMap() {
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementsByTagName('main')[0], {
+    var gMap = document.getElementsByTagName('main')[0], myCenter = {lat: 47.608, lng: -122.33}, mapProp = {
       center: {lat: 47.608, lng: -122.33},
       scrollwheel: true,
       zoom: 8
-    });
-    marker.
-    var infoWindow = new google.maps.InfoWindow({
-        content: 'UW PCE HQ'
-    });
+    }, marker = new google.maps.Marker({
+      position: myCenter
+    }), infoWindow = new google.maps.InfoWindow({
+      content: 'UW PCE HQ'
+    }), map = new google.maps.Map(gMap, mapProp);
+    marker.setMap(map);
   }
 });
